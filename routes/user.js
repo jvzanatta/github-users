8408f981182da2aa05eb929f9edb729e69420cc3
João Victor Zanatta <joaovictor15@gmail.com>
2018-07-13 07:58:21 -0300

    comments

--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,22 @@ const router = express.Router();
 
 const https = require('https');
 
+/**
+ * Token to enable more than 60 requests per hour on GitHub
+ */
 const access_token = 'access_token=eac66f701e75bd91a9d1b132b8c29744c4673efd';
 
+/**
+ * Limit users per page to 15
+ */
 const perPage = 15;
 
+/**
+ * Options for the HTTPS requests
+ */
 let options = {
   hostname: 'api.github.com',
   qs: { access_token: access_token },
-  // path: '/users/',
   port: 443,
   method: 'GET',
   headers: {
@@ -18,21 +26,27 @@ let options = {
   },
 };
 
-/* GET users list */
+/**
+ *  GET user's list from GitHub
+ */
 router.get('/', function(req, res, next) {
-
+  // Path + parameters
   options.path = `/users?per_page=${perPage}&since=${req.query.since}&${access_token}`;
 
   https.get(options, (resp) => {
     let data = '';
-
+    // Collect data until it's finished
     resp.on('data', (chunk) => {
       data += chunk;
     });
 
+    // Assemble responde then send
     resp.on('end', () => {
       data = JSON.parse(data);
+      // Calculates new since variable
       const since = parseInt(req.query.since) + data.length;
+      // Link to next page (GitHub does not provide total users through API
+      // so there is no way to know if the URL will bring more users)
       const nextPageLink = `${req.protocol}://${req.get('host') + req.baseUrl}?since=${since}`;
       const respObject = { link: nextPageLink, users: data };
 
@@ -45,20 +59,22 @@ router.get('/', function(req, res, next) {
 });
 
 
-/* GET user info */
+/**
+ * GET user info
+ */
 router.get('/:username/details', function(req, res, next) {
 
   const username = req.params.username;
-
+  // Path + parameters
   options.path = `/users/${username}?${access_token}`;
 
   https.get(options, (resp) => {
     let data = '';
-
+    // Collect data until it's finished
     resp.on('data', (chunk) => {
       data += chunk;
     });
-
+    // Send response
     resp.on('end', () => {
       res.send(JSON.parse(data));
     });
@@ -68,20 +84,22 @@ router.get('/:username/details', function(req, res, next) {
   });
 });
 
-/* GET user repos */
+/**
+ * GET user's repositories
+ */
 router.get('/:username/repos', function(req, res, next) {
 
   const username = req.params.username;
-
+  // Path + parameters
   options.path = `/users/${username}/repos?${access_token}`;
 
   https.get(options, (resp) => {
     let data = '';
-
+    // Collect data until it's finished
     resp.on('data', (chunk) => {
       data += chunk;
     });
-
+    // Send response
     resp.on('end', () => {
       res.send(JSON.parse(data));
     });
--- server.js
+++ server.js
@@ -9,8 +9,14 @@ const user = require('./routes/user');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/githubusers'));
 
+/**
+ *  API routes to send GitHub data
+ */
 app.use('/api/users', user);
 
+/**
+ * Angular endpoint for the application
+ */
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/githubusers/index.html'));
 });
